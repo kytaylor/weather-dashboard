b8080c13717687573140d4693ee7e4e237ca575e
kytaylor <ky.taylor5468@gmail.com>
2020-12-24 18:04:18 -0800

    adds relevant info to local storage, shows last searched city info if the page is reloaded

--- script.js
+++ script.js
@@ -2,6 +2,47 @@
 var APIKey = "f899658bec757e89390b5ec8e203720c";
 
 // ------------------------------------------------
+// Retrieves last searched info from local storage and displays it
+// $(".city-name").text(localStorage.getItem("weather-city"));
+// $(".wind").text(localStorage.getItem("weather-wind"));
+// $(".humidity").text(localStorage.getItem("weather-humidity"));
+// var newImg = $("<img>")
+// $(newImg).attr("src", localStorage.getItem("weather-icon"));
+// $(".icon").append(newImg);
+// $(".uv-index").text(localStorage.getItem("weather-uvi"));
+// uviColor(localStorage.getItem("weather-uvi"));
+
+$(".temp-one").text(localStorage.getItem("day-one-temp"));
+$(".humidity-one").text(localStorage.getItem("day-one-humidity"));
+var newImg = $("<img>")
+$(newImg).attr("src", localStorage.getItem("day-one-icon"));
+$(".icon-one").append(newImg);
+
+$(".temp-two").text(localStorage.getItem("day-two-temp"));
+$(".humidity-two").text(localStorage.getItem("day-two-humidity"));
+var newImg = $("<img>")
+$(newImg).attr("src", localStorage.getItem("day-two-icon"));
+$(".icon-two").append(newImg);
+
+$(".temp-three").text(localStorage.getItem("day-three-temp"));
+$(".humidity-three").text(localStorage.getItem("day-three-humidity"));
+var newImg = $("<img>")
+$(newImg).attr("src", localStorage.getItem("day-three-icon"));
+$(".icon-three").append(newImg);
+
+$(".temp-four").text(localStorage.getItem("day-four-temp"));
+$(".humidity-four").text(localStorage.getItem("day-four-humidity"));
+var newImg = $("<img>")
+$(newImg).attr("src", localStorage.getItem("day-four-icon"));
+$(".icon-four").append(newImg);
+
+$(".temp-five").text(localStorage.getItem("day-five-temp"));
+$(".humidity-five").text(localStorage.getItem("day-five-humidity"));
+var newImg = $("<img>")
+$(newImg).attr("src", localStorage.getItem("day-five-icon"));
+$(".icon-five").append(newImg);
+
+// ------------------------------------------------
 
 // Initial array of cities
 var cities = ["Portland", "Hollywood"];
@@ -63,6 +104,13 @@ function displayCityInfo() {
     var latitude = response.coord.lat;
     var longitude = response.coord.lon;
 
+    // Adds all necessary info to local storage
+    localStorage.setItem("weather-city", response.name);
+    localStorage.setItem("weather-wind", response.wind.speed);
+    localStorage.setItem("weather-humidity", response.main.humidity);
+    localStorage.setItem("weather-temp", newTempOne);
+    localStorage.setItem("weather-icon", iconLink);
+
     forecast(latitude, longitude)
   });
 
@@ -81,17 +129,10 @@ function forecast(x, y) {
     var uvi = response.current.uvi;
     $(".uv-index").text(uvi);
     // Changing color of UV index depending on uvi value
-    if (uvi >= 0 && uvi <= 2) {
-      $(".uv-index").css({"backgroundColor" : "green"});
-    } else if (uvi >= 3 && uvi <= 5) {
-      $(".uv-index").css({"backgroundColor" : "yellow"});
-    } else if (uvi >= 6 && uvi <= 7) {
-      $(".uv-index").css({"backgroundColor" : "orange"});
-    } else if (uvi >= 8 && uvi <= 10) {
-      $(".uv-index").css({"backgroundColor" : "red"});
-    } else if (uvi >= 11) {
-      $(".uv-index").css({"backgroundColor" : "purple"});
-    }
+    uviColor(uvi);
+
+    // Saves uvi to local storage
+    localStorage.setItem("weather-uvi", uvi);
 
     // Day 1
     var icon = response.daily[0].weather[0].icon;
@@ -105,6 +146,11 @@ function forecast(x, y) {
     $(".temp-one").text(newTempOne);
     $(".humidity-one").text(response.daily[0].humidity);
 
+    // Local storage
+    localStorage.setItem("day-one-icon", iconLink);
+    localStorage.setItem("day-one-temp", newTempOne);
+    localStorage.setItem("day-one-humidity", response.daily[0].humidity);
+
     // Day 2
     var icon = response.daily[1].weather[0].icon;
     var iconLink = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
@@ -116,6 +162,11 @@ function forecast(x, y) {
     var newTempTwo = dayTwoTemp.toFixed(2)
     $(".temp-two").text(newTempTwo);
     $(".humidity-two").text(response.daily[1].humidity);
+
+    // Local storage
+    localStorage.setItem("day-two-icon", iconLink);
+    localStorage.setItem("day-two-temp", newTempTwo);
+    localStorage.setItem("day-two-humidity", response.daily[1].humidity);
 
     // Day 3
     var icon = response.daily[2].weather[0].icon;
@@ -129,6 +180,11 @@ function forecast(x, y) {
     $(".temp-three").text(newTempThree);
     $(".humidity-three").text(response.daily[2].humidity);
 
+    // Local storage
+    localStorage.setItem("day-three-icon", iconLink);
+    localStorage.setItem("day-three-temp", newTempThree);
+    localStorage.setItem("day-three-humidity", response.daily[2].humidity);
+
     // Day 4
     var icon = response.daily[3].weather[0].icon;
     var iconLink = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
@@ -141,6 +197,11 @@ function forecast(x, y) {
     $(".temp-four").text(newTempFour);
     $(".humidity-four").text(response.daily[3].humidity);
 
+    // Local storage
+    localStorage.setItem("day-four-icon", iconLink);
+    localStorage.setItem("day-four-temp", newTempFour);
+    localStorage.setItem("day-four-humidity", response.daily[3].humidity);
+
     // Day 5
     var icon = response.daily[4].weather[0].icon;
     var iconLink = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
@@ -152,9 +213,15 @@ function forecast(x, y) {
     var newTempFive = dayFiveTemp.toFixed(2)
     $(".temp-five").text(newTempFive);
     $(".humidity-five").text(response.daily[4].humidity);
+
+    // Local storage
+    localStorage.setItem("day-five-icon", iconLink);
+    localStorage.setItem("day-five-temp", newTempFive);
+    localStorage.setItem("day-five-humidity", response.daily[4].humidity);
   });
 }
 
+// Function for converting the temperature from Kelvin to Fahrenheit
 function tempConvert(x) {
   var tempK = x;
   console.log(x)
@@ -183,6 +250,21 @@ $("#add-city").on("click", function(event) {
   cities.push(city);
   renderButtons();
 });
+
+// Function for changing color of uvi based on uvi value
+function uviColor(uvi) {
+  if (uvi >= 0 && uvi <= 2) {
+    $(".uv-index").css({"backgroundColor" : "green"});
+  } else if (uvi >= 3 && uvi <= 5) {
+    $(".uv-index").css({"backgroundColor" : "yellow"});
+  } else if (uvi >= 6 && uvi <= 7) {
+    $(".uv-index").css({"backgroundColor" : "orange"});
+  } else if (uvi >= 8 && uvi <= 10) {
+    $(".uv-index").css({"backgroundColor" : "red"});
+  } else if (uvi >= 11) {
+    $(".uv-index").css({"backgroundColor" : "purple"});
+  }
+}
 
 // Adding click event listeners to all elements with a class of "city"
 $(document).on("click", ".city", displayCityInfo);
